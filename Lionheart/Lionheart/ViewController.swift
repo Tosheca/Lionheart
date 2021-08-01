b2318e7ddf7509e7aad2f4a0e2eabfff978a1294
tkp1g20 <tkp1g20@soton.ac.uk>
2021-08-01 17:02:13 +0300

    link commit

---
+++ Lionheart/Lionheart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lionheart
+//
+//  Created by Teodor Pavlov on 1.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
