a49db0617fd178369ed82978923c604f549861b0
Teodor Pavlov <tosheca@icloud.com>
2021-08-03 00:36:50 +0300

    New collectables assets added

---
+++ Lionheart/Lionheart/ViewControllers/PhotoHandler/PhotoHandlerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoHandlerViewController.swift
+//  Lionheart
+//
+//  Created by Teodor Pavlov on 3.08.21.
+//
+
+import UIKit
+
+class PhotoHandlerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
